c0bdffce7339ac15086f1ca44dfc4cd1677f03d1
Romitou <romitou@protonmail.com>
2021-01-22 12:18:24 +0100

    :tada: Initial commit!

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setPublicPath('/build')
+    .setOutputPath('public/build/')
+;
+
+module.exports = Encore.getWebpackConfig();
